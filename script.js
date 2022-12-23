b490ffb324bd87dc8114512b6ddddcec07fc0c94
YITZZIER <119162608+YITZZIER@users.noreply.github.com>
2022-12-23 16:59:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert ('Hoy!');
+alert ('Crush Back');
+alert ('mona ako');
+alert ('habang hindi pa');
+alert ('nag papasko')
+alert ('pag');
+alert ('crinushback');
+alert ('moko');
+alert ('libre kita');
+
+// var Name = "Miguel"
+// var Role = "Developer"
